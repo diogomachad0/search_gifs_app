cb2c2a809595918b20ed3a6f59f961d81a080876
DiogoMachad0 <diogomachado0801@rede.ulbra.br>
2024-06-05 17:55:33 -0300

    update api

---
+++ android/app/src/main/kotlin/com/example/search_gifs_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.search_gifs_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:search_gifs_app/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
